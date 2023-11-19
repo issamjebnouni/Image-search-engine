7f17abc9445c285d1dc0af8830e8dfa12f02d63c
Issam Jebnouni <issam.jebnouni@insat.ucar.tn>
2023-11-19 11:16:02 +0100

    add HOG and GABOR descriptors

--- app/app.py
+++ app/app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import cv2
 import os
-from utils import load_image, calculate_histogram, extract_vgg_features, display_similar_images
-from keras.applications.vgg16 import VGG16
+from utils import load_image, calculate_histogram, calculate_hog, calculate_gabor, display_similar_images
 
 st.set_page_config(
     page_title="Search Engine",
@@ -16,15 +15,13 @@ with st.sidebar:
     st.image(load_image("app/logo.png"), width=100)
     st.header("Parameters")
     k = st.slider('Select the number of images to display', min_value =1, max_value = 10, value = 3)
-    feature_type = st.selectbox("Select feature type:", ["Color Histogram", "VGG-16"])
+    feature_type = st.selectbox("Select feature type:", ["RGB Histogram", "HOG Descriptor", "Gabor Descriptor"])
     distance_type = st.selectbox("Select a metric:", ["Euclidean Distance", "Manhattan Distance", "Cosine Similarity"])
 
 database_images = []
 for filename in os.listdir("database"):
     img = cv2.imread(os.path.join("database", filename))
     database_images.append((f"database/{filename}", img))
-
-model = VGG16(weights='imagenet', include_top=False)
 
 columns = st.columns(k)
 
@@ -42,12 +39,16 @@ if clicked_image_index is not None:
     input_image = cv2.imread(database_images[clicked_image_index][0])
     input_image = cv2.resize(input_image, (200, 200))
 
-    if feature_type == "Color Histogram":
+    if feature_type == "RGB Histogram":
         input_vector = calculate_histogram(input_image)
         database_vectors = [(path, calculate_histogram(image)) for path, image in database_images]
 
-    if feature_type == "VGG-16":
-        input_vector = extract_vgg_features(database_images[clicked_image_index][0], model)
-        database_vectors = [(path, extract_vgg_features(path, model)) for path, image in database_images]
+    if feature_type == "HOG Descriptor":
+        input_vector = calculate_hog(input_image)
+        database_vectors = [(path, calculate_hog(image)) for path, image in database_images]
     
+    if feature_type == "Gabor Descriptor":
+        input_vector = calculate_gabor(input_image)
+        database_vectors = [(path, calculate_gabor(image)) for path, image in database_images]
+
     display_similar_images(distance_type, input_vector, database_vectors)
--- app/utils.py
+++ app/utils.py
@@ -1,10 +1,9 @@
 import cv2
 import numpy as np
 from PIL import Image
-from keras.applications.vgg16 import VGG16, preprocess_input
+from skimage.feature import hog
 import streamlit as st
 
-model = VGG16(weights='imagenet', include_top=False)
 
 def load_image(image_path):
   im = Image.open(image_path)
@@ -15,13 +14,65 @@ def calculate_histogram(image):
     hist = cv2.normalize(hist, hist).flatten()
     return hist
 
-def extract_vgg_features(img_path, model):
-    img = cv2.imread(img_path)
-    img = cv2.resize(img, (224, 224))
-    x = np.expand_dims(img, axis=0)
-    x = preprocess_input(x)
-    features = model.predict(x)
-    return features.flatten()
+def calculate_hog(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    hog1 = hog(gray, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2), visualize=False)
+    return hog1
+
+def get_gabor_kernel(w, h,sigma_x, sigma_y, theta, fi, psi):
+    "getting gabor kernel with those values"
+    # Bounding box
+    kernel_size_x = w
+    kernel_size_y = h
+    (y, x) = np.meshgrid(np.arange(0, kernel_size_y ), np.arange(0,kernel_size_x))
+    # Rotation 
+    x_theta = x * np.cos(theta) + y * np.sin(theta)
+    y_theta = -x * np.sin(theta) + y * np.cos(theta)
+    #Calculate the gabor kernel according the formulae
+    gb = np.exp(-1.0*(x_theta ** 2.0 / sigma_x ** 2.0 + y_theta ** 2.0 / sigma_y ** 2.0)) * np.cos(2 * np.pi * fi * x_theta + psi)
+    return gb
+
+def build_filters(w, h,num_theta, fi, sigma_x, sigma_y, psi):
+    "Get set of filters for GABOR"
+    filters = []
+    for i in range(num_theta):
+        theta = ((i+1)*1.0 / num_theta) * np.pi
+        for f_var in fi:
+            kernel = get_gabor_kernel(w, h,sigma_x, sigma_y, theta, f_var, psi)
+            kernel = 2.0*kernel/kernel.sum()
+            kernel = cv2.normalize(kernel, kernel, 1.0, 0, cv2.NORM_L2)
+            filters.append(kernel)
+    return filters
+
+def extractFeatures(img):
+    "A vector of 2n elements where n is the number of theta angles"
+    "and 2 is the number of frequencies under consideration"
+    filters =  build_filters(img.shape[0],img.shape[1],5,(0.75,1.5),2,1,np.pi/2.0)
+    fft_filters = [np.fft.fft2(i) for i in filters]
+    img_fft = np.fft.fft2(img)
+    a =  img_fft * fft_filters
+    s = [np.fft.ifft2(i) for i in a]
+    k = [p.real for p in s]
+    return k
+
+
+def calculate_gabor(image):
+
+    img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    # Set parameters for Gabor filters
+    w, h = img.shape[1], img.shape[0]
+    num_theta = 5  # Number of orientations
+    fi_values = (0.75, 1.5)  # Frequencies
+    sigma_x, sigma_y = 2, 1  # Standard deviations for x and y
+    psi = np.pi / 2.0  # Phase offset
+
+    # Extract Gabor filters
+    filters = build_filters(w, h, num_theta, fi_values, sigma_x, sigma_y, psi)
+
+    # Extract features from the image using Gabor filters
+    features = extractFeatures(img)
+    return np.array(features).flatten()
 
 def euclidean_distance(vector1, vector2):
     return np.linalg.norm(vector1 - vector2)
